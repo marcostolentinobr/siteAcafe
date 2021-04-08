44cd23df88b7e082ce7fd29194e3475b62e0f36f
marco <marco@192.168.15.6>
2021-04-08 13:59:50 -0300

    parametros separados

--- admin/config.php
+++ admin/config.php
@@ -6,27 +6,10 @@ error_reporting(E_ALL);
 
 session_start();
 
-//BANCO DE DADOS
-//Conteudo do bd.php
-//define('DB_LIB', 'dblib');
+require_once __DIR__ . '/../../../parametros/acafe_bd_site_sa.php';
 
-//Drive
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
-    define('DB_LIB', 'sqlsrv');
-} else {
-    define('DB_LIB', 'dblib');
-}
-
-define('DB_HOST', '10.0.0.3');
-define('DB_NAME', 'WEB_PORTAL');
-define('DB_USER', 'sa');
-define('DB_PASS', 'm0n5t&rw1f12016#');
-define('DB_CHARSET', 'utf8');
-
-//require_once '../../bd_crudPhpMvcPdoJs.php';
 //Title
 define('TITULO', 'Eventos');
-define('DB_CONVERTE_UTF8', TRUE);
 
 //Funções
 require_once 'libs/funcoes.php';
