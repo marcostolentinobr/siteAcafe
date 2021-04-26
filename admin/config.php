80b980f2f4d64c9bd7597fdb795539ea5488afbb
marco <marco@192.168.0.103>
2021-04-26 18:51:25 -0300

    Alterado o nome do arquivo de configuração de banco

--- admin/config.php
+++ admin/config.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 session_start();
 
-require_once __DIR__ . '/../../../parametros/acafe_bd_site_sa.php';
+require_once __DIR__ . '/../../../parametros/bd_site.php';
 
 //Title
 define('TITULO', 'Eventos');
